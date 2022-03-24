d116b939f08fb57fbfff27d2023117b48a9c44d7
Ash2806 <ashnav2806@gmail.com>
2022-03-24 13:21:37 -0230

    Initial commit

---
+++ app/src/main/java/com/example/classtest/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.classtest
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.classtest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        activityMainBinding.playQuizHomeBtn.setOnClickListener {
+            /*
+            * TODO 1: Launch play activity to start playing the quiz.
+             */
+        }
+    }
+}
---
+++ app/src/main/java/com/example/classtest/ResultActivity.kt
@@ -0,0 +1,42 @@
+package com.example.classtest
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.classtest.databinding.ActivityResultBinding
+
+class ResultActivity : AppCompatActivity() {
+    private lateinit var activityResultBinding: ActivityResultBinding
+    var totalScore = 0
+    var correct = 0
+    var wrong = 0
+    var skip = 0
+    var isKey = false
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        /*
+         * TODO 5: Inflate the respective layout, retrieve the intents (keys, value pairs)
+         *   you passed from previous activity to display and initialize the views.
+         */
+    }
+    @SuppressLint("SetTextI18n")
+    private fun initializeViews() {
+        activityResultBinding.apply {
+            score.text = "Score: $totalScore"
+            right.text = "Correct: $totalScore"
+            wrong.text = "Wrong: $wrong"
+            skip.text = "Skip: $skip"
+            /* TODO 6:
+                If the total score is greater than 6 then set the emoji with happy_face and
+                 display a Toast message "Wow Great"
+                 or else show a Toast message "Need Improvement" and set the emoji with blank_face.
+             */
+
+            playAgain.setOnClickListener {
+                finish()
+            }
+        }
+    }
+}
